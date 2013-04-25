72d4955e41f1da1fbeba42455776bb4c5765ea11
argrieve <argireve@iastate.edu>
2013-04-25 17:48:37 -0500

    sandbox.cpp can fully parse a .aut file

--- sandbox.cpp
+++ sandbox.cpp
@@ -1,14 +1,15 @@
-#include <stdlib.h>
-#include <istream>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include "coord.h"
+#include <vector>
+#include <cctype>
 
 using namespace std;
 
 void remove_comments(string &str);
 coord get_range(char c, string &str);
+vector<coord> get_living(string &str);
 
 int main(int argc, char **argv)
 {
@@ -37,10 +38,9 @@ int main(int argc, char **argv)
 	remove_comments(str);
 	coord x = get_range('x', str);
 	coord y = get_range('y', str);
-
-	cout << "X: (" << x.getX() << ", " << x.getY() << ")\n";
-	cout << "Y: (" << y.getX() << ", " << y.getY() << ")\n";
-
+	get_living(str);
+	//cout << "X: (" << x.getX() << ", " << x.getY() << ")\n";
+	//cout << "Y: (" << y.getX() << ", " << y.getY() << ")\n";
 	//cout << str;
 
 	return 0;
@@ -50,11 +50,17 @@ void remove_comments(string &str)
 {
 	unsigned int found1, found2;
 	while (1) {
+		// Find next comment
 		found1 = str.find("#");
 		if (found1 == string::npos || found1 > str.size()) break;
+		
+		// Find the end of the comment
 		found2 = str.find("\n", found1+1);
+
+		// Erase the comment
 		str.erase(found1, found2-found1);
 	}
+	return;
 }
 
 coord get_range(char c, string &str)
@@ -73,4 +79,57 @@ coord get_range(char c, string &str)
 	stream >> low >> high;
 
 	return coord(low, high);	
+}
+
+vector<coord> get_living(string &str)
+{
+	vector<coord> alive;
+	unsigned int start, end;
+
+	// Check if the keyword exists
+	start = str.find("Initial");
+	if (start >= str.size()) return alive; 
+
+	// Find the start and end index of the "Initial" statement
+	start = str.find("{", start+7);
+	end = str.find("}", start+1);
+	// Make sure a complete statement exists
+	if (start >= str.size() || end >= str.size()) return alive;	
+
+	int x, y;
+	bool done = false;
+	istringstream stream(str.substr(start, end-start+1));
+	
+	while (stream.good()) {
+		// Find the next 'Y' char
+		if (stream.peek() == 'Y') {
+			done = false;
+
+			// Process the column of alive cells (current row in .aut file)
+			while (!done) {
+				// Grab the y coordinate
+				if (isdigit(stream.peek()) || stream.peek() == '-') {
+					stream >> y;
+
+					// Grab the x coordinates
+					while (1) {
+						if (isdigit(stream.peek()) || stream.peek() == '-') {
+							stream >> x;	
+							//cout << "( " << y << ", " << x << ")" << endl;
+							alive.push_back(coord(x,y));
+						}
+						else if (stream.peek() == ';') break;
+						else stream.get();
+					} // End x coordinates
+
+					done = true;
+				}
+				else stream.get();
+			} // End processing row
+
+		}
+		else stream.get();
+	}
+
+	return alive;	
 }
--- test.cpp
+++ test.cpp
@@ -1,9 +1,33 @@
 #include <stdlib.h>
 #include <iostream>
-#include <fstream>
+#include <sstream>
+#include <cctype>
 
 using namespace std;
 
+int main()
+{
+	string str("Y = 2 : -1;"); 
+
+	istringstream stream(str);
+
+	int y;
+//	for (int i=0; i<str.size(); i++) {
+	while (stream.good()) {
+		if (isdigit(stream.peek()) || stream.peek() == '-') {
+			stream >> y;
+			cout << "Y: " << y << endl;
+		}
+		else {
+			stream.get();
+		}
+	}
+
+
+	return 0;
+}
+
+/*
 int main()
 {
 	std::ifstream in("test.aut");
@@ -19,4 +43,4 @@ int main()
 
 	return 0;
 }
-
+*/
